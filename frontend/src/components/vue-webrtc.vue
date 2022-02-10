3e594c33f02c79f2cd0c4881c1c9bfb08c0c84fa
ylazo <ylazo@invernaderolabs.com>
2022-02-10 12:36:39 -0600

    actualización 10-02-2022 12.36

--- frontend/src/components/vue-webrtc.vue
+++ frontend/src/components/vue-webrtc.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-const io = require('socket.io-client')
+import { io } from 'socket.io-client'
 const SimpleSignalClient = require('simple-signal-client')
 
 export default {
