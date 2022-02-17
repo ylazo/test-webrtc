85b4f0f83f9d60508a839c83ae5768480a921733
ylazo <ylazo@invernaderolabs.com>
2022-02-16 22:46:20 -0600

    act

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -75,7 +75,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 443,
+      port: 8080,
       open: false // opens browser window automatically
     },
 
