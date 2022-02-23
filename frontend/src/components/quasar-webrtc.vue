87ced71f7fa921942bad8d72b1efceab5fdbbbb7
ylazo <ylazo@invernaderolabs.com>
2022-02-23 11:26:31 -0600

    act

--- frontend/src/components/quasar-webrtc.vue
+++ frontend/src/components/quasar-webrtc.vue
@@ -100,7 +100,8 @@ export default defineComponent({
     }
 
     const join = async () => {
-      const socketUrl = 'http://localhost:3000'
+      // const socketUrl = 'http://localhost:3000'
+      const socketUrl = 'https://fileback.invernaderolabs.com'
       socket.value = io(socketUrl, { rejectUnauthorized: false, transports: ['websocket'] })
       // eslint-disable-next-line
       signalClient.value = new SimpleSignalClient(socket.value)
