9ca2a6b9a2b9775585465e3227c05294f5f68081
ylazo <ylazo@invernaderolabs.com>
2022-02-28 10:04:08 -0600

    act

--- frontend/src/components/quasar-webrtc.vue
+++ frontend/src/components/quasar-webrtc.vue
@@ -111,8 +111,8 @@ export default defineComponent({
     watch(enableVideo, setVideo)
 
     const join = async () => {
-      const socketUrl = 'http://localhost:3000'
-      // const socketUrl = 'https://fileback.invernaderolabs.com'
+      // const socketUrl = 'http://localhost:3000'
+      const socketUrl = 'https://fileback.invernaderolabs.com'
       socket.value = io(socketUrl, { rejectUnauthorized: false, transports: ['websocket'] })
       // eslint-disable-next-line
       signalClient.value = new SimpleSignalClient(socket.value)
